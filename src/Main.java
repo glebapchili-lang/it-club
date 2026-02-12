d19cc048bebf2e77710bda41cabed1ffa8478e61
glebapchili-lang <gleb.ap.chili@gmail.com>
2026-02-12 16:45:36 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        double R = 5;
+        double volume = (4/3) * Math.PI * (R * R * R);
+        System.out.println("ОбЪем = "  + volume);
+        double x = 6;
+        double y = 10;
+        double a = 1 + y;
+        double b =x * x - 4;
+        double c =1 / b;
+        double d = y + c;
+        double e = (x+y) / d;
+        double f = 2 * x;
+        double z = y * y;
+        double v = f + z- e;
+        double result = a * v;
+        System.out.println("Ответ ="+ result);
+        double p = 8;
+        double g = 4;
+        double h = 15;
+        double n = p + 2;
+        double l = g * h;
+        double j = g + l;
+        double t = n * g;
+        double u = t - h;
+        double cat = u / j;
+        System.out.println("Ответ=" + cat);
+    }
+}
